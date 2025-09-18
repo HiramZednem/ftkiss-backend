927e73ea931f5402c8779fa22076424f072acd23
HiramZednem <hirammendez000@gmail.com>
2025-09-18 00:32:56 -0600

    First commit

---
+++ src/main/java/com/codqueto/ftkiss/FtkissApplication.java
@@ -0,0 +1,13 @@
+package com.codqueto.ftkiss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtkissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtkissApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codqueto/ftkiss/FtkissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codqueto.ftkiss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtkissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
